55fa3f36abd5055e5c08c66571419490b41f7da9
burkira <147352689+burakgokmenn@users.noreply.github.com>
2024-12-19 17:18:52 +0300

    Update hikaye.c

--- hikaye.c
+++ hikaye.c
@@ -5,8 +5,7 @@ int main() {
 
 printf("Soğuk bir kış akşamıydı\nKar giderek şiddetleniyordu. 2 kardeş bir an önce kafalarını bir yere sokmak yer arıyorlardı.\n"); //Burak Gökmen hikaye başlangıcı
   printf("Bu bir örnektir uygulamayızı kendi satırınıza alt satıra inmeden bu şekil yazın ");          //ÖRNEK (adın soyadın işte :d ); SOL TARAFINA YAZACAKSINIZ
-          //iREM TOKTAŞ
-          // EMSAL İREM DEMİREL
+  printf("Bir anda bir ses duyuldu.Korkar adımlarla sese doğru yönelmeye başladılar.Küçük kardeş yürüdükçe sese yaklaşıyordu.Abisi her ne kadar ona "GİTME GİTME!"diyerek uyarsa da küçük çocuk çok merak ediyordu ve gitmeye devam ediyordu.);     //iREM TOKTAŞ
     printf("Birden arkalarından gelen bir ses onları durdurdu. "Kim var orada?" diye bağırdılar ama cevap alamadılar. Korkuyla birbirlerine sıkı sıkı sarıldılar.")//İREM TOPSAKAL
           printf("Ormanda kaybolan iki kardeş kendilerine kalacak biryer bulmak için yola koyuldular..");//Mustafa Saput
         printf("malzemeyi arap faikten aldım karabükte ikinci yükleme yapılıyordu adanaya kadarda ben kullanıyordum kamyonu..");  //Yasin ERGÜT
